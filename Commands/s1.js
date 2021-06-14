09f5b8ec68efc7ec7754ad49f8d9587118ba1415
DingleDangleDoff <82572801+DingleDangleDoff@users.noreply.github.com>
2021-06-14 22:30:13 +1000

    Update s1.js

--- Commands/s1.js
+++ Commands/s1.js
@@ -4,6 +4,6 @@ module.exports = {
     execute(Client, message, args, Discord, cmd){
         if (message.channel.type !== "dm") {
             message.delete().catch();
-        } else message.channel.send('https://youtu.be/7tkMvWJJs0g https://cdn.discordapp.com/attachments/853849922906357760/853918111194677278/S2.wav');
+        } else message.channel.send('https://youtu.be/7tkMvWJJs0g https://cdn.discordapp.com/attachments/853849922906357760/853974507285643264/S2.wav');
     }
 }
