3a5085d6442317beaff82067f2191890649ed950
DingleDangleDoff <82572801+DingleDangleDoff@users.noreply.github.com>
2021-06-14 18:49:58 +1000

    Added secret stuff

---
+++ Commands/s1.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 's1',
+    description: "Secret command",
+    execute(Client, message, args, Discord, cmd){
+        if (message.channel.type !== "dm") {
+            message.delete().catch();
+        } else message.channel.send('https://youtu.be/7tkMvWJJs0g https://cdn.discordapp.com/attachments/853849922906357760/853918111194677278/S2.wav');
+    }
+}
