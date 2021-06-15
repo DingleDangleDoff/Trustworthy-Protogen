94e84b2ad177c6403227f37d85d111473e0c4b9b
DingleDangleDoff <82572801+DingleDangleDoff@users.noreply.github.com>
2021-06-15 14:47:35 +1000

    Update s1.js

--- Commands/s1.js
+++ Commands/s1.js
@@ -4,6 +4,6 @@ module.exports = {
     execute(Client, message, args, Discord, cmd){
         if (message.channel.type !== "dm") {
             message.delete().catch();
-        } else message.channel.send('Being remade');
+        } else message.channel.send('https://youtu.be/85uyJfxesY8 https://cdn.discordapp.com/attachments/853849922906357760/854212978169741322/S1.wav');
     }
 }
