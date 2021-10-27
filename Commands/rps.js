e44a946fc6165321dd18b518ca9416523c85c2bf
DingleDangleDoff <82572801+DingleDangleDoff@users.noreply.github.com>
2021-10-27 18:55:04 +1000

    Fixed rock paper scissors

--- Commands/rps.js
+++ Commands/rps.js
@@ -1,8 +1,32 @@
 var object = ['Rock','Paper','Scissors']
+var choices = ['rock','paper','scissors']
 module.exports = {
     name: 'rps',
     description: "Rock, Paper, Scissors",
     execute(Client, message, args, Discord){
-        message.reply(object[Math.floor(Math.random() * object.length)]);
+        const botchoice = object[Math.floor(Math.random() * object.length)]
+        const playerchoice = args[0]
+        
+        if (!playerchoice) return message.channel.send('You did not choose anything, so I automatically win')
+        //if (!choices.includes(playerchoice)) return message.channel.send(`I choose ${botchoice} but because you chose ${playerchoice} witch is not even a proper option, I win`);
+        if (playerchoice.toLowerCase() === botchoice.toLocaleLowerCase()) return message.channel.send(`It's a tie!`)
+        
+        switch (playerchoice.toLowerCase()){
+            case 'rock': {
+                if (botchoice === 'Paper') return message.reply(`You chose ${args[0]}, I chose ${botchoice}, I win`);
+                else return message.reply(`You chose ${args[0]}, I chose ${botchoice}, You win`);
+            }
+            case 'paper': {
+                if (botchoice === 'Scissors') return message.reply(`You chose ${args[0]}, I chose ${botchoice}, I win`);
+                else return message.reply(`You chose ${args[0]}, I chose ${botchoice}, You win`);        
+            }
+            case 'scissors': {
+                if (botchoice === 'Rock') return message.reply(`You chose ${args[0]}, I chose ${botchoice}, I win`);
+                else return message.reply(`You chose ${args[0]}, I chose ${botchoice}, You win`);
+            }
+            default :{
+                return message.channel.send(`I choose ${botchoice} but because you chose ${playerchoice} witch is not even a proper option, I win`);
+            }
+        }
     }
 }
